e3fe06cc9f0a3d16e3ed00284932ecbf83e802bb
João Barbosa <joao.2010pm@gmail.com>
2021-07-20 18:08:11 -0300

    Chess layer and printing the board

---
+++ src/application/UI.java
@@ -0,0 +1,35 @@
+package application;
+
+import chess.ChessPiece;
+
+public class UI {
+	
+	public static void printBoard(ChessPiece[][] pieces) {
+		
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print((8 - i) + " ");
+			for (int j = 0; j < pieces.length; j++) {
+				printPiece(pieces[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println("  a b c d e f g h");
+	}
+	
+	private static void printPiece(ChessPiece piece) {
+		
+		// Operação para validação do conteudo da piece
+		if(piece == null) {
+			System.out.print("-");
+		}
+		else {
+			System.out.print(piece);
+		}
+		
+		// Operador ternário - Utilizando em uma única linha
+		//System.out.print(piece == null ? "-" : piece);
+		
+		System.out.print(" ");
+	}
+
+}

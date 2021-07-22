2943665baa299bc26358613ddb2f0b44a285a9e4
João Barbosa <joao.2010pm@gmail.com>
2021-07-22 16:26:16 -0300

    Moving pieces

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -2,12 +2,12 @@ package boardgame;
 
 public class Piece {
 	
-	protected Position postion;
+	protected Position position;
 	private Board board;
 	
 	public Piece(Board board) {
 		this.board = board;
-		postion = null;
+		position = null;
 	}
 
 	protected Board getBoard() {
